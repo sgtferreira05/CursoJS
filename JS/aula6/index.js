cf5c159ee0678372d04773a0d23a6b1a992dd13c
AILTON <ailtonfsilva05@gmail.com>
2024-10-31 07:12:28 -0300

    att

---
+++ JS/aula6/index.js
@@ -0,0 +1,10 @@
+let name_ = 'Ailton';
+let name1_; // declared the variable, and its value can be modified
+name1_ = 'Andressa'; // inicializing the variable
+
+console.log(name1_);
+console.log(name_, 'was born in 1996');
+console.log('In 2024',name_, 'met Andressa ');
+console.log( name_,'married Andressa in 2030');
+console.log('Andressa had a child with', name_,' in 2030');
+console.log(name_,"'s son's name is Edward ");
---
+++ JS/tests.js
@@ -0,0 +1,5 @@
+console.log('Ailton was born in 1996')
+console.log('In 2024 Ailton met Andressa ')
+console.log('Ailton married Andressa in 2030')
+console.log('Andressa had a child with Ailton in 2030')
+console.log("Ailton's son's name is Edward ")

855e0795f662051afc73b6f472cec3cdea228faa
AILTON <ailtonfsilva05@gmail.com>
2024-11-26 07:29:29 -0300

    att

---
+++ JS/aula10_Aritmetic_increment_and_assignment_operators/index.js
@@ -0,0 +1,59 @@
+
+// Aritmetiics operators
+const num1 = 110;
+const num2 = 10;
+const num3 = '5';
+const num4 = 2;
+const num5 = 'Ailton';
+const num6 = parseInt('10'); //Function to cast a number to an interger
+const num7 = parseFloat('20'); //Function to cast a number to a float
+const num8 = Number('5.2'); //Function to cast a number to a float or interger
+
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % (num4*2));
+console.log(num2 ** num4);
+console.log(num3 + num1); //concatenation
+console.log(num6 + num1, typeof num6);
+console.log(num7 + num1, typeof num7);
+console.log(num8 + num7);
+console.log(num5 * num1); //NaN (Not a Number)
+
+
+let accountant = 1;
+accountant ++;
+accountant ++;
+accountant --;
+++ accountant;
+accountant --;
+++ accountant;
+accountant += 3;
+accountant -= 3;
+accountant *= 3;
+console.log();
+console.log('The accountat value: ', accountant);
+console.log(accountant++);
+console.log(accountant);
+console.log(--accountant);
+
+console.log();
+const step_ = 3;
+let accountant_2 = 1;
+accountant_2 += step_;
+console.log(accountant_2);
+accountant_2 -= step_;
+console.log(accountant_2);
+accountant_2 *= step_;
+console.log(accountant_2);
+accountant_2 *= step_;
+console.log(accountant_2);
+accountant_2 -= step_;
+console.log(accountant_2);
+
+
+
+
+
---
+++ JS/aula11_alert_confirm_n_prompt/js/script.js
@@ -0,0 +1,7 @@
+// window.alert('Our message');
+// window.confirm('Are you sure?');
+// window.prompt('Enter your name: ');
+
+birthyear = Number(prompt('Enter your Year of birth'));
+currentyear = Number(prompt('Enter the current year: '));
+confirm('Do you have ', currentyear - birthyear, ' years old?');

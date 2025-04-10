81387ec0a08192848a1b2f9dbea676a7b760f374
AILTON <ailtonfsilva05@gmail.com>
2025-04-10 19:44:58 -0300

    att

--- JS/exercicio_tabela_imc/assets/js/main.js
+++ JS/exercicio_tabela_imc/assets/js/main.js
@@ -5,6 +5,7 @@ form.addEventListener('submit', function (e) {
 e.preventDefault();
     const inputPeso = e.target.querySelector('#peso');
     const inputAltura = e.target.querySelector('#altura');
+    
     const peso = Number(inputPeso.value);
     const altura = Number(inputAltura.value);
 
